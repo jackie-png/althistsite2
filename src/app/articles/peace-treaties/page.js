89d56a1f57c493985484d61a116ca6c5bdc33c07
jackie-png <jackylam19@hotmail.com>
2024-12-14 12:32:19 -0500

    added possinly final versionof the map on the peace treaty homepage

--- src/app/articles/peace-treaties/page.js
+++ src/app/articles/peace-treaties/page.js
@@ -1,3 +1,4 @@
+import { SelectableCountries2 } from "@/app/components/SelectableCountries2";
 import { SelectableCountries } from "../../components/SelectableCountries";
 
 export default function page(){
@@ -5,7 +6,7 @@ export default function page(){
         <div>
             <div className="border-2 border-solid border-red-500">Peace Treaties</div>
             <div className="min-h-screen bg-[#0B0B2B] border-y-8 border-gray-600 border-solid">
-                <SelectableCountries/>
+                <SelectableCountries2/>
             </div>
         </div>
     )
