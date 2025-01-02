cf642922a2f5fdeef7846bd5c527c470575904b8
jackie-png <jackylam19@hotmail.com>
2025-01-02 14:11:30 -0500

    testing how an article would be put into the website

--- src/app/api/articles/[id]/route.js
+++ src/app/api/articles/[id]/route.js
@@ -1,14 +1,113 @@
 
 const array = [
-    "hi there",
+    {
+        year: 1936,
+        sections:[
+            {
+                sectionName: "The Americas",
+                sectionHeaders:[
+                    {
+                        header:"The United States and Canada",
+                        sectionBody:[
+                            "text text etx tetdsdyskdhhksd sdifhsd kf lkjdsfh sadk f ksd f  fsdjkfhjksdkj fs hdfsdhfsjd f jskfjk sdfh sk dfj sdjfk",
+                            "text kjsdnf sdfj s dfjk;lsfk sf sdfk sdf;jklsjdf l;skdj fsdfkjksdjfskl;d fj sdkfljdslkfjsldkf jsdkfj skfsdkf sfj sdf dskfds f dsfsdkj fsdfksd fsdkjfdk fj sld fjsdkfkslkfkldsklfjlsdlvlkskvjlks dsdf sjkf sf sd fs fsk fsf d fjskl fslf"
+
+                        ]
+                    },
+                    {
+                        header:"The Second American Civil War",
+                        sectionBody:[
+                            "text text etx tetdsdyskdhhksd sdifhsd kf lkjdsfh sadk f ksd f  fsdjkfhjksdkj fs hdfsdhfsjd f jskfjk sdfh sk dfj sdjfk",
+                            "text kjsdnf sdfj s dfjk;lsfk sf sdfk sdf;jklsjdf l;skdj fsdfkjksdjfskl;d fj sdkfljdslkfjsldkf jsdkfj skfsdkf sfj sdf dskfds f dsfsdkj fsdfksd fsdkjfdk fj sld fjsdkfkslkfkldsklfjlsdlvlkskvjlks dsdf sjkf sf sd fs fsk fsf d fjskl fslf"
+
+                        ]
+                    },
+                    {
+                        header:"Other Events in the Americas",
+                        sectionBody:[
+                            "text text etx tetdsdyskdhhksd sdifhsd kf lkjdsfh sadk f ksd f  fsdjkfhjksdkj fs hdfsdhfsjd f jskfjk sdfh sk dfj sdjfk",
+                            "text kjsdnf sdfj s dfjk;lsfk sf sdfk sdf;jklsjdf l;skdj fsdfkjksdjfskl;d fj sdkfljdslkfjsldkf jsdkfj skfsdkf sfj sdf dskfds f dsfsdkj fsdfksd fsdkjfdk fj sld fjsdkfkslkfkldsklfjlsdlvlkskvjlks dsdf sjkf sf sd fs fsk fsf d fjskl fslf"
+
+                        ]
+                    },
+
+                ]
+            },
+            {
+                sectionName: "Europe",
+                sectionHeaders:[
+                    {
+                        header:"The Spanish Civil War",
+                        sectionBody:[
+                            "text text etx tetdsdyskdhhksd sdifhsd kf lkjdsfh sadk f ksd f  fsdjkfhjksdkj fs hdfsdhfsjd f jskfjk sdfh sk dfj sdjfk",
+                            "text kjsdnf sdfj s dfjk;lsfk sf sdfk sdf;jklsjdf l;skdj fsdfkjksdjfskl;d fj sdkfljdslkfjsldkf jsdkfj skfsdkf sfj sdf dskfds f dsfsdkj fsdfksd fsdkjfdk fj sld fjsdkfkslkfkldsklfjlsdlvlkskvjlks dsdf sjkf sf sd fs fsk fsf d fjskl fslf"
+
+                        ]
+                    },
+                    {
+                        header:"The Fourth Balkan War",
+                        sectionBody:[
+                            "text text etx tetdsdyskdhhksd sdifhsd kf lkjdsfh sadk f ksd f  fsdjkfhjksdkj fs hdfsdhfsjd f jskfjk sdfh sk dfj sdjfk",
+                            "text kjsdnf sdfj s dfjk;lsfk sf sdfk sdf;jklsjdf l;skdj fsdfkjksdjfskl;d fj sdkfljdslkfjsldkf jsdkfj skfsdkf sfj sdf dskfds f dsfsdkj fsdfksd fsdkjfdk fj sld fjsdkfkslkfkldsklfjlsdlvlkskvjlks dsdf sjkf sf sd fs fsk fsf d fjskl fslf"
+
+                        ]
+                    },
+                    {
+                        header:"The Leadup to the War",
+                        sectionBody:[
+                            "text text etx tetdsdyskdhhksd sdifhsd kf lkjdsfh sadk f ksd f  fsdjkfhjksdkj fs hdfsdhfsjd f jskfjk sdfh sk dfj sdjfk",
+                            "text kjsdnf sdfj s dfjk;lsfk sf sdfk sdf;jklsjdf l;skdj fsdfkjksdjfskl;d fj sdkfljdslkfjsldkf jsdkfj skfsdkf sfj sdf dskfds f dsfsdkj fsdfksd fsdkjfdk fj sld fjsdkfkslkfkldsklfjlsdlvlkskvjlks dsdf sjkf sf sd fs fsk fsf d fjskl fslf"
+
+                        ]
+                    }
+                ]
+            },
+            {
+                sectionName: "Africa and the Middle East",
+                sectionHeaders:[
+                    {
+                        header:"The War in the Desert",
+                        sectionBody:[
+                            "text text etx tetdsdyskdhhksd sdifhsd kf lkjdsfh sadk f ksd f  fsdjkfhjksdkj fs hdfsdhfsjd f jskfjk sdfh sk dfj sdjfk",
+                            "text kjsdnf sdfj s dfjk;lsfk sf sdfk sdf;jklsjdf l;skdj fsdfkjksdjfskl;d fj sdkfljdslkfjsldkf jsdkfj skfsdkf sfj sdf dskfds f dsfsdkj fsdfksd fsdkjfdk fj sld fjsdkfkslkfkldsklfjlsdlvlkskvjlks dsdf sjkf sf sd fs fsk fsf d fjskl fslf"
+
+                        ]
+                    }
+                ]
+            },
+            {
+                sectionName: "Asia and the Pacific",
+                sectionHeaders:[
+                    {
+                        header:"Japan and China",
+                        sectionBody:[
+                            "text text etx tetdsdyskdhhksd sdifhsd kf lkjdsfh sadk f ksd f  fsdjkfhjksdkj fs hdfsdhfsjd f jskfjk sdfh sk dfj sdjfk",
+                            "text kjsdnf sdfj s dfjk;lsfk sf sdfk sdf;jklsjdf l;skdj fsdfkjksdjfskl;d fj sdkfljdslkfjsldkf jsdkfj skfsdkf sfj sdf dskfds f dsfsdkj fsdfksd fsdkjfdk fj sld fjsdkfkslkfkldsklfjlsdlvlkskvjlks dsdf sjkf sf sd fs fsk fsf d fjskl fslf"
+
+                        ]
+                    },
+                    {
+                        header:"The Indian War of Reunification",
+                        sectionBody:[
+                            "text text etx tetdsdyskdhhksd sdifhsd kf lkjdsfh sadk f ksd f  fsdjkfhjksdkj fs hdfsdhfsjd f jskfjk sdfh sk dfj sdjfk",
+                            "text kjsdnf sdfj s dfjk;lsfk sf sdfk sdf;jklsjdf l;skdj fsdfkjksdjfskl;d fj sdkfljdslkfjsldkf jsdkfj skfsdkf sfj sdf dskfds f dsfsdkj fsdfksd fsdkjfdk fj sld fjsdkfkslkfkldsklfjlsdlvlkskvjlks dsdf sjkf sf sd fs fsk fsf d fjskl fslf"
+
+                        ]
+                    }
+                ]
+            }            
+        ]
+
+
+    },
     "hello there",
     "hey there",
     "bye bye"
 ]
 export async function GET(request){
     const requestUrlSplit = request.nextUrl.pathname.split("/")
-    console.log(requestUrlSplit[requestUrlSplit.length-1])
-    if (requestUrlSplit[requestUrlSplit.length-1] <= array.length && requestUrlSplit.length > 0){
+    console.log(array[requestUrlSplit[requestUrlSplit.length-1]])
+    if (requestUrlSplit[requestUrlSplit.length-1] < array.length && requestUrlSplit.length > 0){
         return new Response(JSON.stringify({message: array[requestUrlSplit[requestUrlSplit.length-1]]}, {headers: { 'Content-Type': 'application/json' }}))
     } else {
         return new Response(null, {status: 400, headers: { 'Content-Type': 'application/json' }})
