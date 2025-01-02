bdfc69548a7a2d9cbbef86fd2d0292f819619c18
jackie-png <jackylam19@hotmail.com>
2025-01-02 15:37:03 -0500

    putting articles from notion to the website

--- src/app/articles/1936-1940/page.js
+++ src/app/articles/1936-1940/page.js
@@ -1,4 +1,3 @@
-"use client"
 import React from "react";
 import { motion } from "framer-motion";
 import ApiComponent from "@/app/components/ApiComponent";
@@ -14,43 +13,16 @@ export default function page(){
                 }}>
                 <div 
                     className="flex justify-center items-center h-screen w-screen absolute top-0 bg-gradient-to-t from-coal from-25%">
-                    <motion.div 
-                        initial={{opacity:0, y: 40}}
-                        animate={{opacity:1, y: 0}}
-                        transition={{
-                            duation: 2,
-                            ease: "easeInOut"
-                        }}
+                    <div 
                         className="bg-coal bg-opacity-60 rounded border-t-soot border-r-soot border-l-soot border-t-2 border-l-2 border-r-2 text-snow flex items-center justify-center flex-col gap-4 h-1/2 w-1/2">
                         <h1 className="text-9xl">1936-1940</h1>
                         <h2 className="text-6xl">The State of The World</h2>
-                    </motion.div>
+                    </div>
                 </div>
             </div>
-            <div className="bg-snow w-9/12 h-full">
+            <div className="bg-snow w-9/12 h-full px-4 py-16 rounded-lg">
                 <ApiComponent/>
             </div>
-            <h1 className="text-white">hi</h1>
-            <h1 className="text-white">hi</h1>
-
-            <h1 className="text-white">hi</h1>
-
-            <h1 className="text-white">hi</h1>
-
-            <h1 className="text-white">hi</h1>
-
-            <h1 className="text-white">hi</h1>
-
-            <h1 className="text-white">hi</h1>
-
-            <h1 className="text-white">hi</h1>
-
-            <h1 className="text-white">hi</h1>
-
-            <h1 className="text-white">hi</h1>
-
-            <h1 className="text-white">hi</h1>
-
         </div>
     )
 }
--- src/app/components/ApiComponent.js
+++ src/app/components/ApiComponent.js
@@ -1,13 +1,38 @@
-export default function ApiComponent(){
-    async function apiCall(){
-        const res = await fetch("http://localhost:3000/api/articles/0")
-        return res
-    }
+import React from "react";
 
-    const response = apiCall().then(
-        (result)=>console.log(result.json().then((result2)=>console.log(result2)))
-    );
+async function apiCall(){
+    const res = await fetch("http://localhost:3000/api/articles/0");
+    const resObj = await res.json();
+    if (res.ok){
+        console.log(resObj)
+        return resObj
+    } else {
+        return null
+    }
+}
+
+export default async function ApiComponent(){
+
+
+    const response = await apiCall()
+    const articleData = response.message
+    console.log(articleData)
     return(
-        <div className="bg-red-500">hi there hello</div>
+        <div className="flex flex-col">
+            {
+                articleData.sections.map((section) => 
+                <div>
+                    <h1 className="bg-darkRuby text-6xl p-4 text-center text-snow font-bold tracking-wide rounded-3xl">{section.sectionName}</h1>
+                    <div>
+                        {section.sectionHeaders.map((header)=>
+                        <div>
+                            <h1 className="text-5xl text-center py-4">{header.header}</h1>
+                            {header.sectionBody.map((bodyText)=><p className="px-8 py-4 text-lg">{bodyText}</p>)}
+                        </div>
+                        )}
+                    </div>
+                </div>)
+            }
+        </div>
     )
 }
