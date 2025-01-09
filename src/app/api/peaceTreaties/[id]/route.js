d0f17f3bae61df741fa0773736ea4481ed2b53dd
jackie-png <jackylam19@hotmail.com>
2025-01-08 20:23:15 -0500

    peace treaty page countries now link to the peace treaty articles

--- src/app/api/peaceTreaties/[id]/route.js
+++ src/app/api/peaceTreaties/[id]/route.js
@@ -2,7 +2,14 @@
 const treaties = new Map(
     [
         ["treaty-of-paris", "THis is the treaty of paris"],
-        ["treaty-of-london", "THis is the treaty of london"]
+        ["treaty-of-london", "THis is the treaty of london"],
+        ["treaty-of-moscow", "THis is the treaty of moscow"],
+        ["treaty-of-beijing", "THis is the treaty of beijing"],
+        ["treaty-of-bangkok", "THis is the treaty of bangkok"],
+        ["treaty-of-ottawa", "THis is the treaty of ottawa"],
+        ["treaty-of-tokyo", "THis is the treaty of tokyo"],
+        ["treaty-of-washington", "THis is the treaty of washington"],
+        ["treaty-of-algiers", "THis is the treaty of algiers"]
     ]
 )
 
--- src/app/articles/peace-treaties/page.js
+++ src/app/articles/peace-treaties/page.js
@@ -2,18 +2,25 @@
 import { SelectableCountries2 } from "@/app/components/SelectableCountries2";
 import { SelectableCountries } from "../../components/SelectableCountries";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 export default function page(){
     const [currentHovered, setCurrentHovered] = useState("")
+    const router = useRouter();
+
+    function goToLink(link){
+        router.push(`/articles/peace-treaties/${link}`)
+    }
 
     useEffect(()=>{
         console.log(currentHovered)
     },[currentHovered])
     return(
         <div>
-            <div className="border-2 border-solid border-red-500">{currentHovered === "" ? "Hover over the map to select a treaty" : currentHovered}</div>
+            <div className="border-2 border-solid border-red-500">{currentHovered === "" ? "Hover over the map to select a treaty" : currentHovered.display}</div>
             <div className="min-h-screen bg-[#0B0B2B] border-y-8 border-gray-600 border-solid">
-                <SelectableCountries2 onHoverStart={setCurrentHovered}/>
+                <SelectableCountries2 onHoverStart={setCurrentHovered} onClick={goToLink}/>
             </div>
+            <button onClick={()=>goToLink("treaty-of-moscow")}>clikc</button>
         </div>
     )
 }
